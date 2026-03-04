3ba300a1735bbebc03aec6bd932ad537aff47244
alonsoF100 <yanul228@mail.ru>
2026-03-04 18:05:14 +0300

    Initial commit

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/alonsoF100/effective-mobile-test
+
+go 1.25.0
---
+++ backend/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello from Effective Mobile!")
+	})
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "OK")
+	})
+
+	log.Println("Backend started on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
